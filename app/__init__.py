cf670ee3f2d2f1fe3fff0c9bbece6d77cfb6f931
edson <edsonnait@gmail.com>
2024-10-22 19:48:07 -0300

    atualizando pastas

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
+# app/__init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Inicializa o app Flask
 app = Flask(__name__)
 
-from app import routes
+# Configurações básicas (adicionar configurações de MySQL mais tarde)
+app.config['SECRET_KEY'] = 'chave_secreta_aqui'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://usuario:senha@localhost/seu_banco_de_dados'
 
+# Inicializa o banco de dados (SQLAlchemy)
+db = SQLAlchemy(app)
+
+from app import routes  # Importa as rotas
+
+# Iniciar a aplicação
+def create_app():
+    return app
 
 
--- semantizar2.py
+++ semantizar2.py
@@ -1,2 +1,9 @@
-from app import app
+from app import create_app
+
+# Cria a instância da aplicação Flask
+app = create_app()
+
+# Executa o servidor se o script for executado diretamente
+if __name__ == '__main__':
+    app.run(debug=True)  # Modo de depuração ativado
 
